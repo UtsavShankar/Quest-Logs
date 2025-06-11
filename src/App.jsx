2f00da07db3243dfd48bf887937fbd3960076f07
select-e <celestephuayx@gmail.com>
2025-06-11 17:52:40 +0800

    Add video background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { auth } from './firebase'; // adjust path as needed
 import {
   onAuthStateChanged
 } from 'firebase/auth';
 import AuthForm from "./components/AuthForm";
 import TodoList from "./components/TodoList";
+import ReactPlayer from 'react-player/vimeo'
 
 function App() {
   const [user, setUser] = useState(null);
@@ -16,6 +17,16 @@ function App() {
 
   return (
     <div style={{ padding: '20px' }}>
+      <ReactPlayer
+          url="https://vimeo.com/1092394026/d857de1950?share=copy"
+          playing
+          loop
+          muted
+          width="100%"
+          height="100%"
+          className="background-video"
+        />
+        <div className="video-overlay"></div>
       {!user ? (
         <AuthForm />
       ) : (
