7f94794000c257c670e394d3ce97c0f28977675a
select-e <celestephuayx@gmail.com>
2025-06-30 11:32:14 +0800

    Set executableName

--- forge.config.js
+++ forge.config.js
@@ -3,6 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
+    executableName: "quest-logs",
     asar: true,
     "extraFiles": [
       "build"
