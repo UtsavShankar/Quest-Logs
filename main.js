d8305744b89266aa7b20c48e72f99748adc8a058
Utsav Shankar <utsaveshan0206@gmail.com>
2025-07-10 14:42:11 +0530

    add tray functionality

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-const { app, BrowserWindow } = require('electron');
+const { create } = require('domain');
+const { app,Tray,Menu, BrowserWindow, nativeImage } = require('electron');
 const path = require('path');
 
 const isDev = !app.isPackaged;
+let tray = null;
+let win = null;
+function createTray() {
+  if (!tray){
+  const icon = nativeImage.createFromPath('' + path.join(__dirname, 'public/favicon.ico'));
+  tray = new Tray(icon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        console.log('Show App clicked');
+        if(!win){
+          createWindow();
+        }else{
+          win.show();
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('This is my application')
+  tray.setTitle('This is my title') //only works on macOS
+  }
+}
+
+
 
 function createWindow() {
-  const win = new BrowserWindow({
+   win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
@@ -18,12 +51,16 @@ function createWindow() {
     ? 'http://localhost:3000'
     : `file://${path.join(__dirname, 'build/index.html')}`
   );
+  createTray();
+  win.on('close', (event) => {
+    event.preventDefault();
+    win.hide();
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  app.quit();
+app.on('window-all-closed', (e) => {
 });
 
 app.on('activate', () => {
