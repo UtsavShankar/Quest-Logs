3a94002a428d65c3d925e280910257d74596099b
Utsav Shankar <utsaveshan0206@gmail.com>
2025-07-22 10:10:54 +0530

    add email verification and password reset

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import { auth } from "../firebase.js";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { FancyButton } from "./Buttons";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
+import { FancyButton, SimpleButton } from "./Buttons";
 import "./Buttons.css";
 
 export default function AuthForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [resetPass, setResetPass] = useState(false);
+  const [resetEmailSent, setResetEmailSent] = useState(false);
+
+  const sendReset = async () => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setResetEmailSent(true);
+      console.log("Reset email sent to", email);
+    }
+    catch (err) {
+      setError(err.message);
+      console.log(err);
+    }
+  };
+
 
   const handleLogin = async () => {
     try {
@@ -18,25 +33,41 @@ export default function AuthForm() {
     }
   };
 
-  const handleRegister = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+ const handleRegister = async () => {
+  try {
+
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    await sendEmailVerification(userCredential.user).then(() => {
+      console.log("Verification email sent to", email);
+    })
+  } catch (err) {
+    setError(err.message);
+    console.log(err);
+  }
+};
 
   return (
+    resetPass ? (
+      <div style={{ position: 'absolute', height: '100%', width: '100%', display:'flex', 
+        flexDirection: 'column', alignItems: 'center', justifyContent: 'center',  gap: '10px'}}>
+        <h2>Reset Password</h2>   
+        <input className="text-input" style={{ width: "200px" }} placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        {resetEmailSent?<SimpleButton>Email Sent</SimpleButton>:<SimpleButton style={{"font-size":"15px"}} onClick={sendReset}>Send Reset Email</SimpleButton>}
+        <SimpleButton style={{"font-size":"15px"}} onClick={() => setResetPass(false)}>Back to Login</SimpleButton>
+        </div>)
+        :(
+  
     <div style={{ position: 'absolute', height: '100%', width: '100%', display:'flex', 
     flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px'}}>
       <h2>Login</h2>
       <input className="text-input" style={{ width: "200px" }} placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
       <input className="text-input" style={{ width: "200px" }} placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <SimpleButton style={{fontSize:"15px"}} onClick={()=> setResetPass(true)}>Did you forget your password?</SimpleButton>
       {<span style={{ color: "red" }}>{error ? error : "\u00A0"}</span>}
       <div style={{display: 'flex', gap: '10px'}}>
         <FancyButton onClick={handleLogin}>Login</FancyButton>
         <FancyButton onClick={handleRegister}>Register</FancyButton>
       </div>
-    </div>
+    </div>)
   );
 }
